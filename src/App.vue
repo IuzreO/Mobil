e17cc3654ad2a45eea12dbde2001d74e1fe91009
何喜 <877073978@qq.com>
2021-03-05 20:22:29 +0800

    项目部署

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <van-cell label="下方">
+      <template #title>
+        <van-icon name="fire-o" />
+        <span>左侧</span>
+        <van-icon name="fire-o" />
+      </template>
+      <template #default>
+        <van-icon name="fire-o" />
+        <span>右侧</span>
+        <van-icon name="fire-o" />
+      </template>
+    </van-cell>
+    <!-- 路由出口 -->
+    <router-view></router-view>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {}
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 页面适配 - flexible
+import 'amfe-flexible'
+// 页面适配 - normalize.css
+import 'normalize.css'
+// 引入Vant
+import Vant from 'vant'
+// 引入Vant.css
+import 'vant/lib/index.css'
+// 使用 vant
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
